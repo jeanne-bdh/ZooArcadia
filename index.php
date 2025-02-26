69296f0ce47b0fd96d56012fbc182228eb436e4a
eurajane <jbouchend@gmail.com>
2025-02-26 17:14:18 +0100

    update front home

--- index.php
+++ index.php
@@ -42,15 +42,15 @@ require_once __DIR__ . "/templates/header.php";
                 <hr class="line-h2">
             </div>
             <div class="d-flex flex-column align-items-center">
-                <a href="/pages/savane.php"><img class="w-75" src="/assets/images/home/habitats/savane-home.jpg" alt="Habitats Savane"></a>
+                <img class="w-75" src="/assets/images/home/habitats/savane-home.jpg" alt="Habitats Savane">
                 <a href="/pages/savane.php"><h4 class="text-black mt-1 mb-4">Savane</h4></a>
             </div>
             <div class="d-flex flex-column align-items-center">
-                <a href="/pages/jungle.php"><img class="w-75" src="/assets/images/home/habitats/jungle-home.jpg" alt="Habitats Jungle"></a>
+                <img class="w-75" src="/assets/images/home/habitats/jungle-home.jpg" alt="Habitats Jungle">
                 <a href="/pages/jungle.php"><h4 class="text-black mt-1 mb-4">Jungle</h4></a>
             </div>
             <div class="d-flex flex-column align-items-center">
-                <a href="/pages/marais.php"><img class="w-75 rounded-md-5" src="/assets/images/home/habitats/marais-home.jpg" alt="Habitats Marais"></a>
+                <img class="w-75 rounded-md-5" src="/assets/images/home/habitats/marais-home.jpg" alt="Habitats Marais">
                 <a href="/pages/marais.php"><h4 class="text-black mt-1 mb-4">Marais</h4></a>
             </div>
         </div>
