4ee047b269e3422381289b256ce4f32cc04e6bf3
eurajane <jbouchend@gmail.com>
2025-08-13 21:45:14 +0200

    crud hours

--- src/Controller/Admin/OpeningHoursCrudController.php
+++ src/Controller/Admin/OpeningHoursCrudController.php
@@ -5,8 +5,8 @@ namespace App\Controller\Admin;
 use App\Entity\OpeningHours;
 use EasyCorp\Bundle\EasyAdminBundle\Controller\AbstractCrudController;
 use EasyCorp\Bundle\EasyAdminBundle\Field\IdField;
-use EasyCorp\Bundle\EasyAdminBundle\Field\TextEditorField;
 use EasyCorp\Bundle\EasyAdminBundle\Field\TextField;
+use EasyCorp\Bundle\EasyAdminBundle\Field\TimeField;
 
 class OpeningHoursCrudController extends AbstractCrudController
 {
@@ -15,14 +15,11 @@ class OpeningHoursCrudController extends AbstractCrudController
         return OpeningHours::class;
     }
 
-    /*
     public function configureFields(string $pageName): iterable
     {
-        return [
-            IdField::new('id'),
-            TextField::new('title'),
-            TextEditorField::new('description'),
-        ];
+        yield IdField::new('id')->hideOnForm();
+        yield TextField::new('day', 'Jour');
+        yield TimeField::new('openTime', 'Heure d\'ouverture');
+        yield TimeField::new('closeTime', 'Heure de fermeture');
     }
-    */
 }
--- src/Controller/HabitatController.php
+++ src/Controller/HabitatController.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Controller\Habitat;
+namespace App\Controller;
 
 use App\Repository\HabitatRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
